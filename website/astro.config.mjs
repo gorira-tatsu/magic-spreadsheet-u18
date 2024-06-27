83b8d1fc19fd94052b6efe7652368beb31370b03
gorira <tatsuhiko.shigoto@gmail.com>
2024-06-27 21:14:32 +0900

    chore: Update deploy workflow to use actions/checkout@v4 and withastro/action@v1

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://gorira-tatsu.github.io',
+    base: '/magic-spreadsheet-u18',
+});
