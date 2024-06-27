cc92f8883606864b2bd798d1bd9c46f8fc4a8bd2
gorira <tatsuhiko.shigoto@gmail.com>
2024-06-27 22:25:21 +0900

    feat: Generate iCal file

--- icalgenerator/generate-ical.js
+++ icalgenerator/generate-ical.js
@@ -6,25 +6,32 @@ import { fileURLToPath } from 'url';
 // ファイルパスの設定
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const listPath = path.join(__dirname, '../website/src/data/list.json');
-const list = JSON.parse(fs.readFileSync(listPath, 'utf-8'));
-
-const cal = ical({ domain: 'yourdomain.com', name: 'My Calendar' });
-
-list.forEach(item => {
-    cal.createEvent({
-        start: new Date(item.AppStartDate),
-        end: new Date(item.AppEndDate),
-        summary: item.Name,
-        description: item.Description,
-        url: item.URL
-    });
-});
-
+const dataDir = path.join(__dirname, '../website/src/content/data');
 const publicDir = path.join(__dirname, '../website/public');
 
+// カレンダーの初期化
+const cal = ical({ domain: 'yourdomain.com', name: 'My Calendar' });
+
+// データディレクトリ内のすべてのJSONファイルを読み込む
+fs.readdirSync(dataDir).forEach(file => {
+    if (path.extname(file) === '.json') {
+        const filePath = path.join(dataDir, file);
+        const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
+
+        // イベントの作成
+        cal.createEvent({
+            start: new Date(data.AppStartDate),
+            end: new Date(data.AppEndDate),
+            summary: data.Name,
+            description: data.Description,
+            url: data.URL
+        });
+    }
+});
+
+// 公開ディレクトリが存在しない場合は作成
 if (!fs.existsSync(publicDir)) {
-    fs.mkdirSync(publicDir);
+    fs.mkdirSync(publicDir, { recursive: true });
 }
 
 // iCalデータを文字列として取得し、ファイルに書き込む
